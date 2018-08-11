984e617cc67debf5ed24378c6b1a7e8a90af99ff
Andrew Friedman <andrewfriedman01@gmail.com>
2018-08-11 00:02:27 -0400

    End of Day 1
    *Started title screen
    *Figured out the majority of the spikes

---
+++ Assets/Code/gameMaster.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameMaster : MonoBehaviour {
+	public GameObject Spikes;
+
+	void Start () {
+		Vector3 Screen_Pos = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0)); // Top right
+		float Screen_Width = Camera.main.orthographicSize * 2f * Screen.width / Screen.height;
+		float Screen_Height = Camera.main.orthographicSize * 2f;
+		float Spikes_Width = Spikes.GetComponent<SpriteRenderer>().size.x * Spikes.GetComponent<Transform>().localScale.x;
+		float Spikes_Height = Spikes.GetComponent<SpriteRenderer>().size.y * Spikes.GetComponent<Transform>().localScale.y;
+
+		int Quantity = Mathf.CeilToInt(Screen_Height / (Spikes.GetComponent<SpriteRenderer>().size.x * Spikes.GetComponent<Transform>().localScale.x)); //fix this
+		for (int i = 0; i < Quantity - 2; i++) { //RIGHT
+			GameObject Clone = Instantiate(Spikes);
+			Clone.name = "Spike #" + i;
+			Clone.GetComponent<Transform>().rotation = Quaternion.Euler(0, 0, 90);
+			float Clone_Scale = Spikes_Width / Spikes.GetComponent<SpriteRenderer>().size.x;
+			Clone.GetComponent<Transform>().localScale = new Vector3(Clone_Scale, Clone_Scale, Clone_Scale);
+			Spikes_Height = Clone.GetComponent<SpriteRenderer>().size.y * Clone.GetComponent<Transform>().localScale.y;
+			if (i == 0) {
+				Spikes_Height = Spikes_Width;
+			}
+			Clone.GetComponent<Transform>().position = new Vector3(Screen_Pos.x - Spikes_Height / 2, Screen_Pos.y - Spikes_Width * (i + .5f), 0);
+		}
+		for (int i = 0; i < Quantity - 2; i++) { //LEFT
+			GameObject Clone = Instantiate(Spikes);
+			Clone.name = "Spike #" + i;
+			Clone.GetComponent<Transform>().rotation = Quaternion.Euler(0, 0, 270);
+			float Clone_Scale = Spikes_Width / Spikes.GetComponent<SpriteRenderer>().size.x;
+			Clone.GetComponent<Transform>().localScale = new Vector3(Clone_Scale, Clone_Scale, Clone_Scale);
+			Spikes_Height = Clone.GetComponent<SpriteRenderer>().size.y * Clone.GetComponent<Transform>().localScale.y;
+			if (i == 0) {
+				Spikes_Height = Spikes_Width;
+			}
+			Clone.GetComponent<Transform>().position = new Vector3(Screen_Pos.x * -1 + Spikes_Height / 2, Screen_Pos.y * -1 + Spikes_Width * (i + .5f), 0);
+		}
+		Quantity = Mathf.FloorToInt(Screen_Width / Spikes_Width);
+		for (int i = 0; i < Quantity - 1; i++) { //TOP
+			GameObject Clone = Instantiate(Spikes);
+			Clone.name = "Spike #" + i;
+			Clone.GetComponent<Transform>().rotation = Quaternion.Euler(0, 0, 180);
+			Spikes_Width = Screen_Width / Quantity;
+			float Clone_Scale = Spikes_Width / Spikes.GetComponent<SpriteRenderer>().size.x;
+			Clone.GetComponent<Transform>().localScale = new Vector3(Clone_Scale, Clone_Scale, Clone_Scale);
+			Spikes_Height = Clone.GetComponent<SpriteRenderer>().size.y * Clone.GetComponent<Transform>().localScale.y;
+			if (i == 0) {
+				Spikes_Height = Spikes_Width;
+			}
+			Clone.GetComponent<Transform>().position = new Vector3(Screen_Pos.x * -1 + (Spikes_Width * (i + .5f)), Screen_Pos.y - Spikes_Height / 2, 0);
+		}
+		for (int i = 0; i < Quantity - 1; i++) { //BOTTOM
+			GameObject Clone = Instantiate(Spikes);
+			Clone.name = "Spike #" + i;
+			Clone.GetComponent<Transform>().rotation = Quaternion.Euler(0, 0, 0);
+			Spikes_Width = Screen_Width / Quantity;
+			float Clone_Scale = Spikes_Width / Spikes.GetComponent<SpriteRenderer>().size.x;
+			Clone.GetComponent<Transform>().localScale = new Vector3(Clone_Scale, Clone_Scale, Clone_Scale);
+			Spikes_Height = Clone.GetComponent<SpriteRenderer>().size.y * Clone.GetComponent<Transform>().localScale.y;
+			if (i == 0) {
+				Spikes_Height = Spikes_Width;
+			}
+			Clone.GetComponent<Transform>().position = new Vector3(Screen_Pos.x - (Spikes_Width * (i + .5f)), Screen_Pos.y * -1 + Spikes_Height / 2, 0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Code/spike.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spike : MonoBehaviour {
+	public Sprite[] Form;
+
+	void Start () {
+		if (this.name == "Spike #0") {
+			this.GetComponent<SpriteRenderer>().sprite = Form[1];
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
